00a5625bf7fed7e25d8eb32ed2f40c2ae6b68bcb
zjply4000 <zjply4000@290fac7e-c239-0410-93cd-3df5dc4d6cd3>
2007-09-10 07:03:26 +0000

    initial checking in

---
+++ trunk/UI.prototype/content/libraries/jquery/_vti_cnf/jquery.treeview.pack.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ZHANGJING\\Administrator
+vti_modifiedby:SR|ZHANGJING\\Administrator
+vti_timelastmodified:TR|10 Sep 2007 06:42:44 -0000
+vti_timecreated:TR|10 Sep 2007 02:50:43 -0000
+vti_extenderversion:SR|12.0.0.4518
+vti_backlinkinfo:VX|setup/organization/index.html
+vti_cacheddtm:TX|10 Sep 2007 06:42:44 -0000
+vti_filesize:IR|1728
+vti_nexttolasttimemodified:TR|10 Sep 2007 02:50:43 -0000
